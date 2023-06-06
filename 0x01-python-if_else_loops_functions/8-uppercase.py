5bb634acb263bad9d254372218b9c1bfd830fb9d
Debby696 <idowuopeyemi696@gmail.com>
2023-06-06 14:14:41 -0700

    a random signed number assigned to variable

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
-    """prints a string in uppercase followed by a new line"""
     for char in str:
         if ord(c) >= 97 and ord(c) <= 122:
             char = chr(ord(c) & ~32)
-            print("{}".format(char), end='')
+            print("{}".format(char), end="")
             print()
