0ab55145389a9d48bcb2cbce1d886643209bf336
Debby696 <idowuopeyemi696@gmail.com>
2023-06-08 07:59:51 -0700

    update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,11 +3,11 @@ if __name__ == "__main__":
     """ a program that prints the number of and the list of its arguments."""
     import sys
     len = len(sys.argv) - 1
-    for i in range(len):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
     if len == 0:
         print("0 arguments")
     elif len == 1:
         print("1 arguments.")
     else:
         print("{} arguments:".format(len))
+        for i in range(len):
+            print("{}: {}".format(i + 1, sys.argv[i + 1]))
