dcf2aa18231b83dd5d62ff32bb48a80b50f3c8bf
Debby696 <idowuopeyemi696@gmail.com>
2023-06-08 09:44:44 -0700

    a program that handles basic mathemaical operation

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""imports all functions from the file calculator_1.py
+and handles basic operations."""
+from calculator_1 import add, sub, mul, div
+import sys
+if len(sys.argv) - 1 != 3:
+    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    sys.exit(1)
+
+    a = int(sys.argv[1])
+    a = int(sys.argv[3])
+    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
