95dd3a9940a3fde15a0d53bc6dcd7a66667c812c
Debby696 <idowuopeyemi696@gmail.com>
2023-06-08 10:02:56 -0700

    Python function def magic_calculation(a, b): that does exactly the same as holberton bytecode

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    """Python function def magic_calculation(a, b): that does exactly the same
+    as bytecode provided by holberton schol"""
+    if b < c:
+        d = add(b, c)
+        for a in range(4, 6):
+            d = add(d, a)
+            return (d)
+        else:
+            return (sub(b, c))
