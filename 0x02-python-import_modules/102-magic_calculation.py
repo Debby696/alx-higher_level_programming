72746b8399152cb46a2bbb114a8f284bc895f6a5
Debby696 <idowuopeyemi696@gmail.com>
2023-06-08 21:41:53 -0700

    Python function def magic_calculation(a, b): that does exactly the same as the following Python bytecode

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -2,6 +2,9 @@
 def magic_calculation(a, b):
     """Python function def magic_calculation(a, b): that does exactly the same
     as bytecode provided by holberton schol"""
+    add = __import__('magic_calculation_102').add
+    sub = __import__('magic_calculation_102').sub
+
     if a < b:
         c = add(a, b)
         for a in range(4, 6):
