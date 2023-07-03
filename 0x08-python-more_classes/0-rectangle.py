b69bf0c19365574e9a9ea10e461b7d08ec2bd6b1
Debby696 <idowuopeyemi696@gmail.com>
2023-07-03 08:20:01 -0700

    a class that defines a rectangle

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,5 +1,7 @@
-#!/usr.bin.python3
+#!/usr/bin/python3
 """an empty class Rectangle that defines a rectangle"""
 
+
 class Rectangle:
-    """this stands for a rectangle pass"""
+    """this stands for a rectangle """
+    pass
