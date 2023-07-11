ad439aae63f013cfb251b763fd07745253ebca85
Debby696 <idowuopeyemi696@gmail.com>
2023-07-10 17:39:54 -0700

    solution to python inheritance

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a base geometry class BaseGeometry  (based on 6-base_geometry.py)"""
+
+
+class BaseGeometry:
+    """signifies a base geometry"""
+
+    def area(self):
+        """this method is not yet implemented"""
+        raise Exception("area() is not implemented")
---
+++ 0x0A-python-inheritance/8-main.py
@@ -0,0 +1,5 @@
+Write a class Rectangle that inherits from BaseGeometry (7-base_geometry.py).
+
+Instantiation with width and height: def __init__(self, width, height):
+width and height must be private. No getter or setter
+width and height must be positive integers, validated by integer_validator
