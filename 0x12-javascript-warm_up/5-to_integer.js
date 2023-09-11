700438bed91c79a49c899f8ec6a9b3405e354234
Debby696 <idowuopeyemi696@gmail.com>
2023-09-11 14:42:46 -0700

    a script that prints an int if the first argument can be converted to an integer

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const { argv } = process;
+
+const firstarg = parseInt(argv[2]);
+
+if (firstarg) {
+  console.log(`My number : ${firstarg}`);
+} else {
+  console.log('Not a number');
+}
