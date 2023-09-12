ab2eccfc3187c1ba8c02a348c3ecbfb0bafc0619
Debby696 <idowuopeyemi696@gmail.com>
2023-09-12 10:07:58 -0700

    a class rectangle that Create an instance method called print

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w >= 1 && h >= 1) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let myVar = '';
+      let y = 0;
+      while (y < this.width) {
+        myVar += 'X';
+        y++;
+      }
+
+      console.log(myVar);
+    }
+  }
+}
+module.exports = Rectangle;
