a3a7b6633754e1b42e0a36a0488c62feb52530d8
Debby696 <idowuopeyemi696@gmail.com>
2023-09-15 08:11:34 -0700

    a script that lists all cities from the database hbtn_0e_4_usa

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""
+a script that lists all cities from the database hbtn_0e_4_usa
+"""
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == '__main__':
+
+    """ this connect to database """
+    db = MySQLdb.connect(
+        host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
+
+    """ create a cursor object """
+    cursor = db.cursor()
+
+    """ write and execute the sql query """
+    cursor.execute("SELECT * FROM cities ORDER BY cities.id ASC")
+
+    """ fetch the results of the query """
+    rows_selected = cursor.fetchall()
+
+    """ print the results """
+    for row in rows_selected:
+        print(row)
