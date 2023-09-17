9caeae6597e07962733617ea6cad3eb3be9f1acc
Debby696 <idowuopeyemi696@gmail.com>
2023-09-17 07:26:13 -0700

    a script that lists all State objects from the database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -9,7 +9,6 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
 
-
     engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
         argv[1], argv[2], argv[3]))
 
@@ -21,5 +20,3 @@ if __name__ == "__main__":
 
         # Close the session
     session.close()
-
-
