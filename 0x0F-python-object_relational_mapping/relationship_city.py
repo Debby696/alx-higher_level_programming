40cedd5cc097f454e8ab50cb3a9779496f0dc633
Debby696 <idowuopeyemi696@gmail.com>
2023-09-17 21:34:52 -0700

    a script that creates the State “California” with the City “San Francisco” from the database

---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""a python file named model_city.py that contains
+the class definition of a City.
+"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from relationship_state import Base, State
+
+
+class City(Base):
+    __tablename__ = 'cities'
+
+    id = Column(Integer, primary_key=True)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    name = Column(String(128), nullable=False)
